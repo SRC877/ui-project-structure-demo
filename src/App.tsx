7192ba0d38b02ffe62475ec6d31f114357598f3a
SRC877 <sanghamitraroychowdhury877@gmail.com>
2022-02-03 09:43:22 +0530

    added grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import { HomeContainer } from './routes/home';
 import { LandingContainer } from './routes/landing';
+import SelectTableComponent from './SelectTable';
 
 const App: React.FC = () => {
     return (
         <>
             <Switch>
                 <Route path="/home" render={() => <HomeContainer />}></Route>
-                <Route path="/" render={() => <LandingContainer />}></Route>
+                {/* <Route path="/" render={() => <LandingContainer />}></Route> */}
+                <Route path="/" render={() => <SelectTableComponent renderHeader={true} />}></Route>
             </Switch>
         </>
     );
